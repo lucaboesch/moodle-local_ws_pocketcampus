e1f272dce04891cab4738e4275f0b37a08d10c9b
Luca Bösch <luca.boesch@bfh.ch>
2021-03-11 12:37:50 +0100

    Token script added.
    Signed-off-by: Luca Bösch <luca.boesch@bfh.ch>

--- pocketcampus_token.php
+++ pocketcampus_token.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - https://moodle.org/
+// This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,13 +12,83 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * TODO.
- *
+ * Return token using a shared secret to verify request source.
  * @package     local_ws_pocketcampus
  * @copyright   2021 Amer Chamseddine <amer@pocketcampus.org>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
+define('AJAX_SCRIPT', true);
+define('REQUIRE_CORRECT_ACCESS', true);
+define('NO_MOODLE_COOKIES', true);
+
+require_once(dirname(dirname(__FILE__)) . '../../config.php');
+require_once($CFG->libdir . '/externallib.php');
+
+$pcid = required_param('pcsecret', PARAM_RAW);
+$userid = required_param('user_id', PARAM_ALPHANUMEXT);
+$serviceshortname  = required_param('service',  PARAM_ALPHANUMEXT);
+$config = get_config('local_ws_pocketcampus');
+
+echo $OUTPUT->header();
+
+// Check if the service exists and is enabled.
+$service = $DB->get_record('external_services', array('shortname' => $serviceshortname, 'enabled' => 1));
+if (empty($service)) {
+    if ($serviceshortname === 'moodle_mobile_app') {
+        throw new moodle_exception('enablewsdescription', 'webservice');
+    } else {
+        throw new moodle_exception('servicenotavailable', 'webservice');
+    }
+}
+
+if ($pcid === $config->secret) {
+    $user = get_complete_user_data('username', $userid);
+} else {
+    throw new moodle_exception('invalidsecret', 'local_ws_pocketcampus');
+}
+
+$systemcontext = context_system::instance();
+if (!empty($user)) {
+
+    // Cannot authenticate unless maintenance access is granted.
+    $hasmaintenanceaccess = has_capability('moodle/site:maintenanceaccess', $systemcontext, $user);
+    if (!empty($CFG->maintenance_enabled) and !$hasmaintenanceaccess) {
+        throw new moodle_exception('sitemaintenance', 'admin');
+    }
+    if (isguestuser($user)) {
+        throw new moodle_exception('noguest');
+    }
+    if (empty($user->confirmed)) {
+        throw new moodle_exception('usernotconfirmed', 'moodle', '', $user->username);
+    }
+    // Check credential expiry.
+    $userauth = get_auth_plugin($user->auth);
+    if (!empty($userauth->config->expiration) and $userauth->config->expiration == 1) {
+        $days2expire = $userauth->password_expire($user->username);
+        if (intval($days2expire) < 0 ) {
+            throw new moodle_exception('passwordisexpired', 'webservice');
+        }
+    }
+    // Setup user session to check capability.
+    \core\session\manager::set_user($user);
+    // Get an existing token or create a new one.
+    $token = external_generate_token_for_current_user($service);
+    external_log_token_request($token);
+    $usertoken = new stdClass;
+    $usertoken->token = $token->token;
+    // Private token, only transmitted to https sites and non-admin users.
+    $siteadmin = has_capability('moodle/site:config', $systemcontext, $USER->id);
+    if (is_https() and !$siteadmin) {
+        $usertoken->privatetoken = $token->privatetoken;
+    } else {
+        $usertoken->privatetoken = null;
+    }
+    echo json_encode($usertoken);
+} else {
+    throw new moodle_exception('usernamenotfound', 'moodle');
+}
--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ws_pocketcampus';
-$plugin->release = '0.1.0';
-$plugin->version = 2021031000;
+$plugin->release = '0.9.0';
+$plugin->version = 2021031100;
 $plugin->requires = 2020110900;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_BETA;
