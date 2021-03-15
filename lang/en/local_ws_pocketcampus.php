bced9947b059b6ff93a3d09603d454e8d4f74467
Luca Bösch <luca.boesch@bfh.ch>
2021-03-15 12:13:01 +0100

    Adding subnet setting.

--- lang/en/local_ws_pocketcampus.php
+++ lang/en/local_ws_pocketcampus.php
@@ -29,5 +29,8 @@ $string['invalidsecret'] = 'The secret is invalid.';
 $string['secret'] = 'PocketCampus secret';
 $string['secretdesc'] = 'The PocketCampus secret';
 $string['pluginname'] = 'BFH PocketCampus Authentication Webservice';
+$string['requiresubnet'] = 'Require network address';
+$string['requiresubnet_desc'] = 'Webservice access may be restricted to particular subnets on the LAN or Internet by specifying a comma-separated list of partial or full IP address numbers. Thus, only people in a certain location can access the service.';
+$string['subnetwrong'] = 'This webservice is only accessible from certain locations, and this computer is not on the allowed list.';
 $string['privacy:metadata'] = 'The local_ws_pocketcampus plugin does not store any personal data.';
 
